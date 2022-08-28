2f7324eaee67d4822136ba61d6148fd899844c8d
Veritoo123 <112337712+Veritoo123@users.noreply.github.com>
2022-08-28 12:37:09 -0500

    Creando repositorio

---
+++ src/main/java/Parte2/PruebaFiguras.java
@@ -0,0 +1,17 @@
+package Parte2;
+
+
+public class PruebaFiguras {
+    public static void main(String args[]){
+        //Creación de figuras
+        Circulo figura1= new Circulo(2);
+        
+        
+        
+        
+        
+        //Imprimimos calculos hechos a las figuras
+        System.out.println("El área del círculo es= " + figura1.calcularArea());
+    }
+    
+}
